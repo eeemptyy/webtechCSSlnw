c387d8de73993ddbb3f60ab170ea3489f75ea12e
5710404357 <guitarr_gs@hotmail.com>
2017-03-26 00:01:09 +0700

    click editTable button , load data

--- js/guitar-ad.js
+++ js/guitar-ad.js
@@ -1,39 +1,51 @@
 
-    //data for province combobox
 $(document).ready(function(){
+ 
+    var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value=""> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Role</th><th></th></tr></thead><tbody>';
 
-var data = [ [ 5610404452, "John" , "Smith" , "Student" ]];
-
-var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value=""> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Position</th><th></th></tr></thead><tbody>';
-for (var i = 0, len = data.length; i < len; ++i) {
-    html += '<tr>';
-    html += '<td><input class="checkbox" type="checkbox" value=""></td>'
-    for (var j = 0, rowLen = data[i].length; j < rowLen; ++j ) {
-        
-        html += '<td>' + data[i][j] + '</td>';
-    }
-    html += '<td><input type="button" id="edit" alt="Edit" data-toggle="modal" data-target="#editModal" value="" value=""><input type="button" id="delete" alt="Delete" data-toggle="modal" data-target="#deleteModal" value=""></td>';
-    html += "</tr>";
+    $.ajax({
+    type: "POST",
+    url: "controller/switcher.php",
+    data: {
+        func: "get_user"
+        },
+    success: function(data) {
+        var obj = JSON.parse(data);
+        for (var i = 0, len = obj.length; i < len; ++i) {
+        var objIn = obj[i];
+        html += '<tr>';
+        html += '<td><input type="checkbox" value=""></td><td>' + objIn['username'] + '</td><td>' + objIn['firstname'] + '</td><td>' + objIn['lastname'] + '</td><td>' + objIn['role'] + '</td>';
+        html += '<td><input type="button" id="edit"  value="" onclick="editTable(this)" alt="Edit" data-toggle="modal" data-target="#editModal"><input type="button" id="delete" alt="Delete" data-toggle="modal" data-target="#deleteModal" value=""></td>';
+        html += "</tr>";
 }
 html += '</tbody><tfoot><tr></tr></tfoot></table>';
+  $(html).appendTo('#table');          
+            
+    },
+    error: function(data) {
+        alert("error "+data);
+    }
 
-$(html).appendTo('#table');
-
-
-//mint's section
-    $("#select_all").change(function(){ 
-    	$(".checkbox").prop('checked', $(this).prop("checked"));
-	});
-	$('.checkbox').change(function(){ 
-    	if(false == $(this).prop("checked")){
-        	$("#select_all").prop('checked', $(this).prop("checked")); 
-    	}
-    	if ($('.checkbox:checked').length == $('.checkbox').length ){
-        	$("#select_all").prop('checked', true);
-    	}
-	});
-
+}); 
+    
 });
-   
 
-alt="Edit" data-toggle="modal" data-target="#editModal"
+
+    
+function editTable(button) {
+    var currentID = $(button).parents('tr').find('td');
+    $("#usr").val(currentID.eq(1).html());
+    $("#firstname").val(currentID.eq(2).html());
+    $("#lastname").val(currentID.eq(3).html());
+    $("#role").val(currentID.eq(4).html());
+}
+  
+
+
+
+
+
+
+
+
+   
