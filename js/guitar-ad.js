3d5ac9b8220329e157c0fc0e168e65979285430d
mint <poramain.k@ku.th>
2017-03-25 19:34:48 +0700

    mint: add select all checkbox function in guitar-ad.js

--- js/guitar-ad.js
+++ js/guitar-ad.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 
 var data = [ [ 5610404452, "John" , "Smith" , "Student" ]];
 
-var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" value=""> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Position</th><th></th></tr></thead><tbody>';
+var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value=""> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Position</th><th></th></tr></thead><tbody>';
 for (var i = 0, len = data.length; i < len; ++i) {
     html += '<tr>';
-    html += '<td><input type="checkbox" value=""></td>'
+    html += '<td><input class="checkbox" type="checkbox" value=""></td>'
     for (var j = 0, rowLen = data[i].length; j < rowLen; ++j ) {
         
         html += '<td>' + data[i][j] + '</td>';
@@ -18,7 +18,20 @@ for (var i = 0, len = data.length; i < len; ++i) {
 html += '</tbody><tfoot><tr></tr></tfoot></table>';
 
 $(html).appendTo('#table');
-    
+
+
+//mint's section
+    $("#select_all").change(function(){ 
+    	$(".checkbox").prop('checked', $(this).prop("checked"));
+	});
+	$('.checkbox').change(function(){ 
+    	if(false == $(this).prop("checked")){
+        	$("#select_all").prop('checked', $(this).prop("checked")); 
+    	}
+    	if ($('.checkbox:checked').length == $('.checkbox').length ){
+        	$("#select_all").prop('checked', true);
+    	}
+	});
 
 });
    
