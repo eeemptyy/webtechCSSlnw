2f5bc034839d07b8fef35a24bf97fc2f096995be
5710451118 <aomaomonly@gamil.com>
2017-03-26 17:18:54 +0700

    add select all check box function

--- js/guitar-ad.js
+++ js/guitar-ad.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
  
-    var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value=""> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Role</th><th></th></tr></thead><tbody>';
+    var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value="" onclick="toggleCheck()"> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Role</th><th></th></tr></thead><tbody>';
 
     $.ajax({
     type: "POST",
@@ -14,7 +14,7 @@ $(document).ready(function(){
         for (var i = 0, len = obj.length; i < len; ++i) {
         var objIn = obj[i];
         html += '<tr>';
-        html += '<td><input type="checkbox" value=""></td><td>' + objIn['username'] + '</td><td>' + objIn['firstname'] + '</td><td>' + objIn['lastname'] + '</td><td>' + objIn['role'] + '</td>';
+        html += '<td><input class="childbox" type="checkbox" value=""></td><td>' + objIn['username'] + '</td><td>' + objIn['firstname'] + '</td><td>' + objIn['lastname'] + '</td><td>' + objIn['role'] + '</td>';
         html += '<td><input type="button" id="edit"  value="" onclick="editTable(this)" alt="Edit" data-toggle="modal" data-target="#editModal"><input type="button" id="delete" alt="Delete" data-toggle="modal" data-target="#deleteModal" value=""></td>';
         html += "</tr>";
 }
@@ -43,7 +43,22 @@ function editTable(button) {
 
 
 
-
+function toggleCheck(){
+    var parBox = document.getElementById('select_all');
+    if(parBox.checked == true){
+        var chbox = document.getElementsByClassName('childbox');
+        for(var i = 0, len = chbox.length; i < len; ++i){
+            chbox[i].checked = true;
+        }  
+    }
+    else{
+        var chbox = document.getElementsByClassName('childbox');
+        for(var i = 0, len = chbox.length; i < len; ++i){
+            chbox[i].checked = false;
+        }
+    }
+    
+}
 
 
 
