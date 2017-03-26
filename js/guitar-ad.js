d6832ad56c20c7e5cd6920ec1e2fb6fd27fcb604
Boonyapornn <naampuengz@gmail.com>
2017-03-26 22:22:41 +0700

    add QR code page n'mome and add Datatable to admin page

--- js/guitar-ad.js
+++ js/guitar-ad.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function(){
- 
-    var html = '<table class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value="" onclick="toggleCheck()"> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Role</th><th></th></tr></thead><tbody>';
+
+    var html = '<table id="mydatatable" class="table table-hover table-bordered"><thead><tr><th><input type="checkbox" id="select_all" value="" onclick="toggleCheck()"> All</th><th>Username ID</th><th>Firstname</th><th>Lastname</th><th>Role</th><th></th></tr></thead><tbody>';
+
 
     $.ajax({
     type: "POST",
@@ -10,24 +11,34 @@ $(document).ready(function(){
         func: "get_user"
         },
     success: function(data) {
-        var obj = JSON.parse(data);
-        for (var i = 0, len = obj.length; i < len; ++i) {
-        var objIn = obj[i];
-        html += '<tr>';
-        html += '<td><input class="childbox" type="checkbox" value=""></td><td>' + objIn['username'] + '</td><td>' + objIn['firstname'] + '</td><td>' + objIn['lastname'] + '</td><td>' + objIn['role'] + '</td>';
-        html += '<td><input type="button" id="edit"  value="" onclick="editTable(this)" alt="Edit" data-toggle="modal" data-target="#editModal"><input type="button" id="delete" alt="Delete" data-toggle="modal" data-target="#deleteModal" value=""></td>';
-        html += "</tr>";
-}
-html += '</tbody><tfoot><tr></tr></tfoot></table>';
-  $(html).appendTo('#table');          
+            var obj = JSON.parse(data);
+            for (var i = 0, len = obj.length; i < len; ++i) {
+                    var objIn = obj[i];
+                    html += '<tr>';
+                    html += '<td><input class="childbox" type="checkbox" value=""></td><td>' + objIn['username'] + '</td><td>' + objIn['firstname'] + '</td><td>' + objIn['lastname'] + '</td><td>' + objIn['role'] + '</td>';
+                    html += '<td><input type="button" id="edit"  value="" onclick="editTable(this)" alt="Edit" data-toggle="modal" data-target="#editModal"><input type="button" id="delete" alt="Delete" data-toggle="modal" data-target="#deleteModal" value=""></td>';
+                    html += "</tr>";
+            }
+            html += '</tbody><tfoot><tr></tr></tfoot></table>';
+            $(html).appendTo('#table');
+            $('#mydatatable').DataTable({
+              "order": [[1, "asc"]],
+              // "pageLenght": 25,
             
-    },
-    error: function(data) {
-        alert("error "+data);
-    }
+              destroy: true,
+              "autoWidth": false,
+              "paging":   false,
+              "ordering": false,
+              "info":     false
 
-}); 
-    
+            });
+    },
+          error: function(data) {
+              alert("error "+data);
+          }
+
+      });
+
 $("#modal-submit").click(function() {
      var userid = $('#userid_create').val();
      var fname = $('#fname_create').val();
@@ -51,20 +62,20 @@ $("#modal-submit").click(function() {
                  success : function(data){
 //                     alert("success "+data);
                      location.reload();
-                     
+
                  }
              });
          }
         else {
             return false;
         }
-     
- }); 
-    
+
+ });
+
 });
 
 
-    
+
 function editTable(button) {
     var currentID = $(button).parents('tr').find('td');
     $("#usr").val(currentID.eq(1).html());
@@ -72,7 +83,7 @@ function editTable(button) {
     $("#lastname").val(currentID.eq(3).html());
     $("#role").val(currentID.eq(4).html());
 }
-  
+
 
 
 
@@ -82,7 +93,7 @@ function toggleCheck(){
         var chbox = document.getElementsByClassName('childbox');
         for(var i = 0, len = chbox.length; i < len; ++i){
             chbox[i].checked = true;
-        }  
+        }
     }
     else{
         var chbox = document.getElementsByClassName('childbox');
@@ -90,17 +101,17 @@ function toggleCheck(){
             chbox[i].checked = false;
         }
     }
-    
+
 }
 function isValid(text) {
-   
-    
+
+
     if(/^[A-Za-z]+$/.test(text) ){
         return true;
     }
     else {
         return false;
-        
+
     }
 }
 
@@ -110,13 +121,6 @@ function isID(text) {
     }
     else {
         return false;
-        
+
     }
 }
-
-
-
-
-
-
-   
